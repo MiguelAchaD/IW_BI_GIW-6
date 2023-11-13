f925ebad8cd5baed1ae971af54aec1ff5eb54cc5
c0xrn3r <iddemiguel03@gmail.com>
2023-11-13 12:40:29 +0100

    .

--- Proyecto/web/app/static/js/finalBuild/build.js
+++ Proyecto/web/app/static/js/finalBuild/build.js
@@ -1,8 +1,14 @@
 var lastSelection;
 var productType = document.getElementsByClassName("clickableOption");
 
-    
+document.addEventListener("DOMContentLoaded", function() {
+    for (var i = 0; i<productType.length;i++){
+        productType[i].addEventListener("change"+[i].toString(), function() {
+            changeSelection(productType[i].attributes());
+        });
+    }
+});
 
 function changeSelection(type){
-    
+    console.log(type["id"]);
 }
